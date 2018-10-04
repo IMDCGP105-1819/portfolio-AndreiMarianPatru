05f59637c2b30be68a3dfada65cafbc764d3ea07
AndreiMarianPatru <43639547+AndreiMarianPatru@users.noreply.github.com>
2018-10-04 11:39:55 +0100

    task 2 input in python

---
+++ PythonApplication2.py
@@ -0,0 +1,21 @@
+name=input("enter your name: ")
+age=int(input("enter your age: "))
+height=int(input("enter your height in cm: "))
+weight=int(input("enter your weight in kilograms: "))
+eye=input("enter your eye colour: ")
+hair=input("enter your hair collor: ")
+if age<5:
+    print("you are a baby")
+elif age>80:
+    print("you are an old man")
+if height<50:
+    print("you are a short man")
+elif height>180:
+    print("you are a tall man")
+if weight<50:
+    print("you should eat smt")
+elif weight>120:
+    print("you should go to gym")
+
+
+
