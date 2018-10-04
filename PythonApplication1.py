7e2d5801e7bc3caf427c859282b22537bf4fa53d
AndreiMarianPatru <43639547+AndreiMarianPatru@users.noreply.github.com>
2018-10-04 11:16:03 +0100

    Add files via upload
    first app in python

---
+++ PythonApplication1.py
@@ -0,0 +1,6 @@
+n=99
+while n>0:
+    print("bottles of beer on the wall,"+str(n)+" bottles of beer. ")
+    n-=1
+    print("take one down, pass it around,"+str(n)+" bottles of beer on the wall...)")
+print("take it down, pass it around, no more bottles of beer on the wall!")
