94e993649597b6a46bdf3c03fa587443b22487cd
AndreiMarianPatru <43639547+AndreiMarianPatru@users.noreply.github.com>
2018-10-18 11:20:54 +0100

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,5 +1,6 @@
-
-for i in range(0,101):
+n=int(input("enter low value: "))
+m=int(input("enter high value: "))
+for i in range(n,m+1):
     if i%3==0 and i%5==0:
         print("fizzbuzz")
     elif i%3==0:
