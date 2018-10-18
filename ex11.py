1bf24ec827d972f75400c4f3fc03832c483590d1
AndreiMarianPatru <43639547+AndreiMarianPatru@users.noreply.github.com>
2018-10-18 11:30:36 +0100

    worksheet 4 task 2

---
+++ ex11.py
@@ -0,0 +1,14 @@
+import random
+guesses=0
+n=random.randint(0,100)
+#guess=input("enter your guess ")
+print(n)
+while True:
+    guess=int(input("enter your guess "))
+    if guess==n:
+        print("that is the answer")
+        print("number of guesses: "+str(guesses))
+        break
+    else:
+        guesses+=1
+       
