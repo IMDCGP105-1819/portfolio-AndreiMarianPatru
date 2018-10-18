2b4add6b91ec96a6d6582227b1bc1a5c7fb359ac
AndreiMarianPatru <43639547+AndreiMarianPatru@users.noreply.github.com>
2018-10-18 11:21:32 +0100

    Delete PythonApplication2.py

--- PythonApplication2.py
+++
@@ -1,8 +0,0 @@
-name=input("enter your name: ")
-annual_salary=int(input("enter your annual salary: "))
-portion_saved=int(input("how much you want to save each month (%)): "))
-total_cost=inext(input("how much your dream house costs: "))
-current_savings=int(input("enter your current savings: "))
-
-portion_deposit=float(total_cost*0.2)
-whi
