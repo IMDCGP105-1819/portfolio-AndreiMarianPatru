edcd5d453e57c36a720cc0ca243bb4e2bc9718e5
AndreiMarianPatru <s193300@uos.ac.uk>
2019-01-31 15:53:24 +0000

    week 1 ex 4

---
+++ week 1/ex4.py
@@ -0,0 +1,18 @@
+# replace these with your own values!
+my_nameZ = 'Andrei Patru'
+my_ageZ = 19 # maybe
+my_heightZ = 74 # inches
+my_heightToCm=my_heightZ*2.54
+my_weightZ = 200 # pounds
+my_weightToKg=my_weightZ*0.45
+my_eyesZ = 'Brown'
+my_hairZ = 'Brown'
+is_heavyZ = my_weightZ > 3000
+print(f"Let's talk about {my_nameZ}.")
+# swap in your preferred pronoun.
+print(f"He is {my_heightZ} inches tall.")
+print(f"He's {my_weightZ} pounds heavy.")
+print(f"It is {is_heavyZ} that he is overweight.")
+print(f"He's got {my_eyesZ} eyes and {my_hairZ} hair.")
+totalZ = my_ageZ + my_heightZ + my_weightZ
+print(f"If I add {my_ageZ}, {my_heightZ} and {my_weightZ} I get {totalZ}")
