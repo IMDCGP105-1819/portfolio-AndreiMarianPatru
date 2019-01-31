7856d1590a00296827bbcfc769c73f8edb91db9a
AndreiMarianPatru <s193300@uos.ac.uk>
2019-01-31 16:06:49 +0000

    week 2 ex 5

---
+++ week2/ex5.py
@@ -0,0 +1,7 @@
+i=99
+while i>=2:
+    print(str(i)+" bottles of beer on the wall, "+str(i)+" bottles of beer.")
+    print("Take one down, pass it around, "+str(i-1)+" bottles of beer on the wall…")
+    i-=1
+print("1 bottle of beer on the wall, 1 bottle of beer!")
+print("So take it down, pass it around, no more bottles of beer on the wall!")
