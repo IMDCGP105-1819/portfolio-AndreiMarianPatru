ed5f07a49f2c9ba123365b2c881accd445e808e9
AndreiMarianPatru <s193300@uos.ac.uk>
2019-02-02 18:09:06 +0000

    Update poker.py
    added a deck class, but I can't remove 2 11 cards

--- week 9/poker.py
+++ week 9/poker.py
@@ -1,7 +1,25 @@
 import random
 
 class Card:
-    def __init__(self,colour,type):
+    def __init__(self,colour,type,number):
         self.colour=colour
         self.type=type
-        
+        self.number=number
+class Deck(Card):
+    def __init__(self):
+        pass
+    def get_cards(self):
+        deck=[]
+        for i in range(1,15):
+            deck.append(Card("red","heart",i))
+            deck.append(Card("red","diamond",i))
+            deck.append(Card("black","club",i))
+            deck.append(Card("black","spade",i))
+        for i in deck:
+            if i.number==11:
+                deck.remove(i)
+        return deck
+a=Deck()
+b=a.get_cards()
+for x in b:
+    print(x.number," ",x.colour," ",x.type)
