f6debd15d85f5ea8a76b59b58586e1e73c1e8fee
AndreiMarianPatru <s193300@uos.ac.uk>
2019-02-03 09:58:19 +0000

    added hand class
    added hand class but not working properly

--- week 9/poker.py
+++ week 9/poker.py
@@ -6,8 +6,8 @@ class Card:
         self.type=type
         self.number=number
 class Deck(Card):
-    def __init__(self):
-        pass
+    def __init__(self,deck):
+        self.deck=deck
     def get_cards(self):
         deck=[]
         for i in range(1,15):
@@ -15,11 +15,33 @@ class Deck(Card):
             deck.append(Card("red","diamond",i))
             deck.append(Card("black","club",i))
             deck.append(Card("black","spade",i))
+        random.shuffle(deck)
         for i in deck:
             if i.number==11:
                 deck.remove(i)
         return deck
-a=Deck()
-b=a.get_cards()
-for x in b:
+class Hand(Deck):
+    def __init__(self,deck):
+        self.deck=deck
+    deckCopy=deck
+    random.shuffle(deckCopy)
+    def get_hand():
+        Hand=[]
+        counter=1
+        for i in deckCopy:
+            Hand.append(i)
+            deckCopy.remove(i)
+            if counter==5:
+                break
+        return Hand
+
+
+
+myDeck=Deck()
+MyCards=myDeck.get_cards()
+#for x in MyCards:
+#    print(x.number," ",x.colour," ",x.type)
+myHand=Hand()
+randvar=myHand.get_hand()
+for x in randvar:
     print(x.number," ",x.colour," ",x.type)
