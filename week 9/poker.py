946f9683d6600a37c480cfe6e51e5f479caf3050
AndreiMarianPatru <s193300@uos.ac.uk>
2019-02-05 18:04:22 +0000

    poker.py is now done, fixed hand class

--- week 9/poker.py
+++ week 9/poker.py
@@ -1,47 +1,43 @@
 import random
-
+#card class used for creating cards objects
 class Card:
     def __init__(self,colour,type,number):
         self.colour=colour
         self.type=type
         self.number=number
-class Deck(Card):
-    def __init__(self,deck):
-        self.deck=deck
-    def get_cards(self):
-        deck=[]
-        for i in range(1,15):
-            deck.append(Card("red","heart",i))
-            deck.append(Card("red","diamond",i))
-            deck.append(Card("black","club",i))
-            deck.append(Card("black","spade",i))
-        random.shuffle(deck)
-        for i in deck:
-            if i.number==11:
-                deck.remove(i)
-        return deck
-class Hand(Deck):
-    def __init__(self,deck):
-        self.deck=deck
-    deckCopy=deck
-    random.shuffle(deckCopy)
+#deck class whill create a 52 card-objects list
+class Deck():
+    def __init__(self):
+        pass
+    deck=[]
+    for i in range(1,15):
+        deck.append(Card("red","heart",i))
+        deck.append(Card("red","diamond",i))
+        deck.append(Card("black","club",i))
+        deck.append(Card("black","spade",i))
+    random.shuffle(deck)
+    #removing cards with number 11 because 1 and 11 cards are the same
+    for i in deck:
+        if i.number==11:
+            deck.remove(i)
+#hand class creates hands of 5 cards, taking items from the list created in deck class
+class Hand():
+    def __init__(self):
+        pass
+
     def get_hand():
-        Hand=[]
+        copydeck=Deck.deck[:]
+        random.shuffle(copydeck)
         counter=1
-        for i in deckCopy:
-            Hand.append(i)
-            deckCopy.remove(i)
+        #show the hands created using an empty line after each hand
+        for i in copydeck:
+            print('{} {} {}'.format(i.number,i.type,i.colour))
             if counter==5:
-                break
-        return Hand
+                print()
+                counter=0
+            counter+=1
+
+        print("No more cards to deal")
 
 
-
-myDeck=Deck()
-MyCards=myDeck.get_cards()
-#for x in MyCards:
-#    print(x.number," ",x.colour," ",x.type)
-myHand=Hand()
-randvar=myHand.get_hand()
-for x in randvar:
-    print(x.number," ",x.colour," ",x.type)
+Hand.get_hand()
