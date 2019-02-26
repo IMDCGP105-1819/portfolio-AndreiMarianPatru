04cd0f894c1114e3005e9b4f8237aa2dd035d66a
AndreiMarianPatru <s193300@uos.ac.uk>
2019-02-26 09:50:37 +0000

    implemented a new approach towards classes,  made user able to deal hands and  commenting

--- week 9/poker.py
+++ week 9/poker.py
@@ -1,43 +1,52 @@
 import random
+from itertools import islice
 #card class used for creating cards objects
-class Card:
-    def __init__(self,colour,type,number):
-        self.colour=colour
-        self.type=type
-        self.number=number
+class Card():
+  def __init__(self, color, suit, rank):
+    self.color = color
+    self.suit = suit
+    self.rank = rank
+    #display a card sensibly
+  def display(self):
+    print( '{2}-{0}-{1}'.format(self.color, self.suit,self.rank))
 #deck class whill create a 52 card-objects list
 class Deck():
-    def __init__(self):
-        pass
-    deck=[]
+  def __init__(self):
+
+    self.deck=[]
     for i in range(1,15):
-        deck.append(Card("red","heart",i))
-        deck.append(Card("red","diamond",i))
-        deck.append(Card("black","club",i))
-        deck.append(Card("black","spade",i))
-    random.shuffle(deck)
-    #removing cards with number 11 because 1 and 11 cards are the same
-    for i in deck:
-        if i.number==11:
-            deck.remove(i)
-#hand class creates hands of 5 cards, taking items from the list created in deck class
-class Hand():
-    def __init__(self):
-        pass
 
-    def get_hand():
-        copydeck=Deck.deck[:]
-        random.shuffle(copydeck)
-        counter=1
-        #show the hands created using an empty line after each hand
-        for i in copydeck:
-            print('{} {} {}'.format(i.number,i.type,i.colour))
-            if counter==5:
-                print()
-                counter=0
-            counter+=1
-
-        print("No more cards to deal")
+      if i != 11:#1 and 11 are the same in a deck
+        self.deck.append(Card("red","heart",i))
+        self.deck.append(Card("red","diamond",i))
+        self.deck.append(Card("black","club",i))
+        self.deck.append(Card("black","spade",i))
+    #make our deck random
+    random.shuffle(self.deck)
 
 
-Hand.get_hand()
+#hand class creates hands of 5 cards, taking items from the deck created above
+  def get_hand( theDeck ):
+    #check if there are enough cards to make a new hand
+    l = len(theDeck.deck)
+    if l < 5:
+      print("No more hands to deal")
+    else:
+      print("Press e to deal a hand")
+      #get the first 5 items from our deck
+      iterator = islice(theDeck.deck, 5)
+      remain = slice(5,l)
+      theDeck.deck = theDeck.deck[remain]
+      #show the hand created
+      for i in iterator:
+        i.display()
+
+#create an instance of a deck
+theDeck = Deck()
+#make the player able to deal hands
+print("Press e to deal a hand")
+while True:
+    ans=input()
+    if ans=="e":
+        Hand.get_hand(theDeck)
+        print()
